dade0ae9254e1164ad1d2a5b1b1d0404c317efd8
firatkilinc7 <firatkilinc7@outlook.com>
2022-11-24 21:19:00 +0300

    user model, active check helper added

--- application/helpers/tools_helper.php
+++ application/helpers/tools_helper.php
@@ -1,18 +1,26 @@
 <?php 
-	
+	//admin panelde ayarlanan ayarlarının front tarafında kullanılması
 	function get_settings(){
 
-    $t = &get_instance();
+		$t = &get_instance();
+		$t->load->model("settings_model");
+		$settings = $t->settings_model->get();
+		$t->session->set_userdata("settings", $settings);
 
-    $t->load->model("settings_model");
+		return $settings;
+	}
+	
+	//user login olup olmadığını denetle
+	function get_active_user(){
 
-    $settings = $t->settings_model->get();
-
-    $t->session->set_userdata("settings", $settings);
-
-    return $settings;
-}
-
+		$t = &get_instance();
+		$user = $t->session->userdata("user");
+		if($user){
+			return $user;
+		}else{
+			return false;
+		}
+	}
 
 
 
