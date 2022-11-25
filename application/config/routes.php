401eb7aa62f568b1baf1dfa516bbe6281e023f14
firatkilinc7 <firatkilinc7@outlook.com>
2022-11-25 21:50:18 +0300

    ekibimiz add back end added

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,9 +6,16 @@ $route['translate_uri_dashes'] = FALSE;
 $route['404_override']       = 'home/error_page';
 $route['default_controller'] = 'home';
 
+/*
+ * ADMIN PANEL ISLEMLERI
+ */
 
-//admin panel işlemleri
-$route["login"] = "userop/login";
-$route["logout"]          = "userop/logout";
-$route["teams"] = "teams/index";
+//Giriş İşlemleri
+$route["login"]     = "userop/login";
+$route["logout"]    = "userop/logout";
+
+//Ekibimiz Bölümü
+$route["teams"]     = "teams/index";
+$route["teams/add"] = "teams/new_form";
+$route["teams/save"] = "teams/save";
 
--- application/controllers/Teams.php
+++ application/controllers/Teams.php
@@ -34,6 +34,123 @@ class Teams extends CI_Controller
 
         $this->load->view("{$viewData->frontViewFolder}/{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
+	
+	public function new_form(){
+
+        $viewData = new stdClass();
+
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+        $viewData->frontViewFolder = "admin";
+
+
+        $this->load->view("{$viewData->frontViewFolder}/{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+    }
+	
+	public function save(){
+
+        $this->load->library("form_validation");
+		
+        if($_FILES["img_url"]["name"] == ""){
+
+            $alert = array(
+                "title" => "İşlem Başarısız",
+                "text" => "Lütfen bir görsel seçiniz",
+                "type"  => "error"
+            );
+
+            $this->session->set_flashdata("alert", $alert);
+
+            redirect(base_url("teams/add"));
+            die();
+        }
+		
+		
+        $this->form_validation->set_rules("name", "Ad Soyad", "required|trim");
+        $this->form_validation->set_rules("role", "Rol", "required|trim");
+        $this->form_validation->set_rules("rank", "Rank", "required|numeric|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required"  => "<b>{field}</b> alanı doldurulmalıdır.",
+                "numeric"  => "<b>{field}</b> alanı sayısal değer içermelidir."
+            )
+        );
+
+        $validate = $this->form_validation->run();
+		
+        if($validate){
+			
+			
+            $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
+
+            $image_900x600 = upload_picture($_FILES["img_url"]["tmp_name"], "uploads/admin/$this->viewFolder",900,600, $file_name);
+
+            if($image_900x600){
+
+                $insert = $this->our_teams_model->add(
+                    array(
+                        "name"         => $this->input->post("name"),
+                        "role"   => $this->input->post("role"),
+                        "img_url"       => $file_name,
+                        "rank"          => $this->input->post("rank"),
+                        "isActive"      => 1,
+                        "createdAt"     => date("Y-m-d H:i:s")
+                    )
+                );
+
+                // TODO Alert sistemi eklenecek...
+                if($insert){
+
+                    $alert = array(
+                        "title" => "İşlem Başarılı",
+                        "text" => "Kayıt başarılı bir şekilde eklendi",
+                        "type"  => "success"
+                    );
+
+                } else {
+
+                    $alert = array(
+                        "title" => "İşlem Başarısız",
+                        "text" => "Kayıt Ekleme sırasında bir problem oluştu",
+                        "type"  => "error"
+                    );
+                }
+
+            } else {
+
+                $alert = array(
+                    "title" => "İşlem Başarısız",
+                    "text" => "Görsel yüklenirken bir problem oluştu",
+                    "type"  => "error"
+                );
+
+                $this->session->set_flashdata("alert", $alert);
+
+                redirect(base_url("teams/add"));
+
+                die();
+
+            }
+
+            $this->session->set_flashdata("alert", $alert);
+
+            redirect(base_url("teams"));
+
+        } else {
+
+            $viewData = new stdClass();
+
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "add";
+            $viewData->frontViewFolder = "admin";
+            $viewData->form_error = false;
+			
+            $this->load->view("{$viewData->frontViewFolder}/{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+
+    }
 
 
 }
