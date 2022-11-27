292b5cb4e9960b5a2a8598b7d7c450691c260ba9
firatkilinc7 <firatkilinc7@outlook.com>
2022-11-27 19:44:43 +0300

    send mail helper added

--- application/helpers/tools_helper.php
+++ application/helpers/tools_helper.php
@@ -83,4 +83,44 @@
 
 	}
 	
+	//email gönderme servisi
+	function send_email($toEmail = "", $subject = "26-40-62 SOFT", $message = ""){
+
+    $t = &get_instance();
+
+    $t->load->model("email_model");
+
+    $email_settings = $t->email_model->get(
+        array(
+            "isActive"  => 1
+        )
+    );
+
+    if(empty($toEmail))
+        $toEmail = $email_settings->to;
+
+     $config = array(
+
+        "protocol"   => $email_settings->protocol,
+        "smtp_host"  => $email_settings->host,
+        "smtp_port"  => $email_settings->port,
+        "smtp_user"  => $email_settings->user,
+        "smtp_pass"  => $email_settings->password,
+        "starttls"   => true,
+        "charset"    => "utf-8",
+        "mailtype"   => "html",
+        "wordwrap"   => true,
+        "newline"    => "\r\n"
+    );
+
+    $t->load->library("email", $config);
+
+    $t->email->from($email_settings->from, $email_settings->user_name);
+    $t->email->to($toEmail);
+    $t->email->subject($subject);
+    $t->email->message($message);
+
+    return $t->email->send();
+    
+}
 ?>
