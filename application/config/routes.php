38ac8c3a067b7e0435d7a6e78d41d64caf200893
firatkilinc7 <firatkilinc7@outlook.com>
2022-11-28 01:57:35 +0300

    admin email settings backend

--- application/config/routes.php
+++ application/config/routes.php
@@ -12,18 +12,27 @@ $route['default_controller'] = 'home';
 
 
 	//Giriş İşlemleri
-	$route["login"]     = "userop/login";
-	$route["logout"]    = "userop/logout";
-	$route["reset/password"]    = "userop/forget_password";
+	$route["login"]                         = "userop/login";
+	$route["logout"]                        = "userop/logout";
+	$route["reset/password"]                = "userop/forget_password";
+	$route["reset/send-mail"]               = "userop/reset_password";
 
 
 	//Ekibimiz Bölümü
-	$route["teams"]     = "teams/index";
-	$route["teams/add"] = "teams/new_form";
-	$route["teams/save"] = "teams/save";
-	$route["teams/delete/(:any)"]    = "teams/delete/$1";
-	$route["teams/update_form/(:any)"]    = "teams/update_form/$1";
+	$route["teams"]                         = "teams/index";
+	$route["teams/add"]                     = "teams/new_form";
+	$route["teams/save"]                    = "teams/save";
+	$route["teams/delete/(:any)"]           = "teams/delete/$1";
+	$route["teams/update_form/(:any)"]      = "teams/update_form/$1";
 	
 	
 	//Blog Bölümü
-	$route["blogs"]    = "blogs/index";
+	$route["blogs"]                         = "blogs/index";
+	
+	
+	//Settings & E-Mail Bölümü
+	$route["settings/email"]                = "email/index";
+	$route["settings/email/new_form"]       = "email/new_form";
+	$route["settings/email/save"]           = "email/save";
+	$route["settings/email/delete/(:any)"]  = "email/delete/$1";
+	
