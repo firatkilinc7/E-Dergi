a7aeceea2179fae33d85198cf16399f8ea23cd06
firatkilinc7 <firatkilinc7@outlook.com>
2022-11-30 19:17:16 +0300

    admin sidebar permission update

--- application/views/admin/includes/aside.php
+++ application/views/admin/includes/aside.php
@@ -1,3 +1,8 @@
+<?php $user_permission = get_user_permission()?>
+
+
+
+
 <aside id="sidebar-left" class="sidebar-left">
 	
 	<div class="sidebar-header">
@@ -12,21 +17,49 @@
 			<nav id="menu" class="nav-main" role="navigation">
 				<ul class="nav nav-main">
 					
-					<li>
-						<a href="<?php echo base_url('dashboard') ?>">
-							<i class="fa fa-home" aria-hidden="true"></i>
-							<span>Ana Sayfa</span>
-						</a>
-					</li>
+					<!-- BÜTÜN KULLANICILAR İÇİN GÖZÜKÜR!!-->
+					<?php if($user_permission>0){?>
+						
+						<li>
+							<a href="<?php echo base_url('dashboard') ?>">
+								<i class="fa fa-home" aria-hidden="true"></i>
+								<span>Ana Sayfa</span>
+							</a>
+						</li>
+					
+					<?php }?>
+					
+					
+					<!-- ADMİN ve EDİTÖR İÇİN GÖZÜKÜR!!-->
+					<?php if($user_permission>1){?>
+					
+						<li><a href="<?php echo base_url('blogs'); ?>"><i class="fa fa-file" aria-hidden="true"></i>Bloglar</a></li>
+					
+					<?php }?>
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					<!-- SADECE ADMİN İÇİN GÖZÜKÜR!!-->
+					<?php if($user_permission>2){?>
 					
 					<li><a href="<?php echo base_url('teams'); ?>"><i class="fa fa-male" aria-hidden="true"></i>Ekibimiz</a></li>
 					
-					<li><a href="<?php echo base_url('blogs'); ?>"><i class="fa fa-file" aria-hidden="true"></i>Bloglar</a></li>
-					
 					<li><a href="<?php echo base_url('settings/email'); ?>"><i class="fa fa-envelope" aria-hidden="true"></i>E-Posta Ayarları</a></li>
-					
-					<li><a href="<?php echo base_url('settings'); ?>"><i class="fa fa-gears" aria-hidden="true"></i>Site Ayarları</a></li>
+
+					<li><a href="<?php echo base_url('users'); ?>"><i class="fa fa-users" aria-hidden="true"></i>Kullanıcılar</a></li>
 				
+					<li><a href="<?php echo base_url('settings'); ?>"><i class="fa fa-gears" aria-hidden="true"></i>Site Ayarları</a></li>
+					
+					<?php }?>
+					
 				</ul>
 			</nav>
 			
