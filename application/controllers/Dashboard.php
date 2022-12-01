c437e165f2b9d7f6a8e574e958c34cee5a83e105
firatkilinc7 <firatkilinc7@outlook.com>
2022-12-02 02:13:38 +0300

    Admin dashboard pie graphs
    Co-Authored-By: firatsadikoglu <84281851+firatsadikoglu@users.noreply.github.com>
    Co-Authored-By: MLHYLMZ <melihyilmaz2645@gmail.com>

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -10,7 +10,12 @@ class Dashboard extends CI_Controller {
         parent::__construct();
 
         $this->viewFolder = "dashboard_v";
+		
+		$this->load->model("blogs_model");
+		$this->load->model("user_model");
         
+		$user_permission = get_user_permission();
+		
         if(!get_active_user()){
             redirect(base_url("login"));
         }
@@ -19,11 +24,69 @@ class Dashboard extends CI_Controller {
 
     public function index()
 	{
-
-	    $viewData = new stdClass();
-        $viewData->viewFolder = $this->viewFolder;
+		$blog_array_active = array();
+		$blog_array_deactive = array();
+		
+		if(get_user_permission()==1){
+			
+			$blog_array_active = array(
+				"isActive"  => 1,
+				"author"    => get_user_name()
+			);
+			
+			$blog_array_deactive = array(
+				"isActive"  => 0,
+				"author"    => get_user_name()
+			);
+		}else{
+			
+			$blog_array_active = array(
+				"isActive"  => 1,
+			);
+			
+			$blog_array_deactive = array(
+				"isActive"  => 0,
+				"author"    => get_user_name()
+			);
+		}
+		
+		$num_blogs_active = $this->blogs_model->get_count($blog_array_active);
+		
+		$num_blogs_deactive = $this->blogs_model->get_count($blog_array_deactive);
+		
+		$num_admin = $this->user_model->get_count(
+			
+			array(
+				"type"  => 'admin'
+			)
+		);
+		
+		$num_editor = $this->user_model->get_count(
+			
+			array(
+				"type"  => 'editor'
+			)
+		);
+		
+		$num_anon = $this->user_model->get_count(
+			
+			array(
+				"type"  => 'anon'
+			)
+		);
+		
+		$viewData = new stdClass();
+        $viewData->viewFolder      = $this->viewFolder;
         $viewData->frontViewFolder = "admin";
-
+		
+        $viewData->num_blogs_active    = $num_blogs_active;
+        $viewData->num_blogs_deactive  = $num_blogs_deactive;
+        $viewData->num_admin           = $num_admin;
+        $viewData->num_editor  		   = $num_editor;
+        $viewData->num_anon   		   = $num_anon;
+		
+		
 		$this->load->view("{$viewData->frontViewFolder}/{$viewData->viewFolder}/index", $viewData);
+		
 	}
 }
--- application/views/admin/dashboard_v/content.php
+++ application/views/admin/dashboard_v/content.php
@@ -1,8 +1,65 @@
+<?php 
+$user_permission = get_user_permission();
+$text4anon = "Yazılarım";
+$text4editor = "Yazılar";
+?>
+
 <section class="panel">
     <header class="panel-heading">
         <h2 class="panel-title">Dashboard</h2>
     </header>
     <div class="panel-body">
-		<p>DASHBOARD</p>
+		<div id="pie_yazilar" style="float:left"></div>
+		<?php if($user_permission > 2){ ?>
+			<div id="pie_uyeler" style="float:left"></div>
+		<?php } ?>
 	</div>
 </section>
+
+
+
+
+
+<script type="text/javascript">
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Onaylandı', <?php echo $num_blogs_active; ?>],
+  ['Onay Bekliyor', <?php echo $num_blogs_deactive; ?>]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title': <?php echo get_user_permission() == 1 ? "\"Yazılarım\"" : "\"Yazılar\"" ?>, 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('pie_yazilar'));
+  chart.draw(data, options);
+}
+</script>
+<script type="text/javascript">
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart2);
+
+// Draw the chart and set the chart values
+function drawChart2() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Admin', <?php echo $num_admin; ?>],
+  ['Editör', <?php echo $num_editor; ?>],
+  ['Anon', <?php echo $num_anon; ?>]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'Üye Durumu', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('pie_uyeler'));
+  chart.draw(data, options);
+}
+</script>
