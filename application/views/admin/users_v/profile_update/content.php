c24773e7758968a741cf34208bf0af1fd7b9da99
MLHYLMZ <melihyilmaz2645@gmail.com>
2022-12-04 15:51:40 +0300

    admin panel update form added

---
+++ application/views/admin/users_v/profile_update/content.php
@@ -0,0 +1,49 @@
+<div class="row">
+	<div class="col-lg-12">
+		<section class="panel">
+			<header class="panel-heading">
+				<div class="panel-actions"> </div>
+				<h2 class="panel-title"><em>Profilinizi düzenliyorsunuz.</em> </h2>
+			</header>
+			<div class="panel-body">
+				<form class="form-horizontal form-bordered" action="<?php echo base_url("profile/update/$item->id"); ?>" method="post" method="post">
+
+					<div class="form-group">
+						<label class="col-md-2 control-label">Kullanıcı Adı</label>
+						<div class="col-md-10">
+							<input class="form-control" placeholder="Kullanıcı Adı" name="user_name" value="<?php echo isset($form_error) ? set_value("user_name") : $item->user_name ; ?>">
+							<?php if(isset($form_error)){ ?>
+								<small class="pull-right input-form-error"> <?php echo form_error("user_name"); ?></small>
+							<?php } ?>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-2 control-label">Ad Soyad</label>
+						<div class="col-md-10">
+							<input class="form-control" placeholder="Ad Soyad" name="full_name" value="<?php echo isset($form_error) ? set_value("full_name") : $item->full_name; ?>">
+							<?php if(isset($form_error)){ ?>
+								<small class="pull-right input-form-error"> <?php echo form_error("full_name"); ?></small>
+							<?php } ?>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="col-md-2 control-label">E-posta Adresi</label>
+						<div class="col-md-10">
+							<input class="form-control" type="email" placeholder="E-posta Adresi" name="email" value="<?php echo isset($form_error) ? set_value("email") : $item->email; ?>">
+							<?php if(isset($form_error)){ ?>
+								<small class="pull-right input-form-error"> <?php echo form_error("email"); ?></small>
+							<?php } ?>
+						</div>
+					</div>
+		
+					<div class="form-group ">
+						<div class="col-md-3">
+							<input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+							<button type="submit" class="btn btn-primary btn-md">Güncelle</button>
+							<a href="<?php echo base_url("users"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+						</div>
+					</div>
+				</form>
+			</div>
