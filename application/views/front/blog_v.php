8f1f1e240b129feb8caf8a4ba73dbd9e91ccca9c
MLHYLMZ <melihyilmaz2645@gmail.com>
2022-12-06 00:19:50 +0300

    Article page front-end

---
+++ application/views/front/blog_v.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<?php $this->load->view("front/{$viewFolder}/seo"); ?>
+	<?php $this->load->view('front/includes/include_style'); ?>
+</head>
+<body>
+	<div id="pozo-page-wrapper">
+		<!-- Lines -->
+		<div class="content-lines-wrapper">
+			<div class="content-lines-inner">
+				<div class="content-lines"></div>
+			</div>
+		</div>
+
+		<div class="body">
+			<?php $this->load->view('front/includes/header'); ?>
+
+			<?php $this->load->view("{$frontViewFolder}/{$viewFolder}/content"); ?>
+			
+			<?php $this->load->view('front/includes/footer'); ?>
+
+			<?php $this->load->view('front/includes/include_script'); ?>
+
+			
+		</body>
+		</html>
